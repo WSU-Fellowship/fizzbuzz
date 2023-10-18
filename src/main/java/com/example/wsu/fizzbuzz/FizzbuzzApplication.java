d44c53e96250632b23197d30362b4268a48aaaae
WSU-Fellowship <148389594+WSU-Fellowship@users.noreply.github.com>
2023-10-18 16:47:42 -0400

    initial commit

---
+++ src/main/java/com/example/wsu/fizzbuzz/FizzbuzzApplication.java
@@ -0,0 +1,16 @@
+package com.example.wsu.fizzbuzz;
+
+public class FizzbuzzApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Running FizzBuzz for 100 numbers!\n");
+		for(int i = 1; i <= 100; i++) {
+			System.out.println(nextNumber(i));
+		}
+	}
+
+	public static Object nextNumber(int number) {
+		// todo: implementation
+		return number;
+	}
+}
